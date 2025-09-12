de1caf9a74a0a411362e2aa5afd3691bde974562
Cubimal Chik <shubhachakrala@gmail.com>
2025-09-12 15:13:23 -0400

    I am starting to learn DSA

---
+++ src/dsacoursetim/HelloWorld.java
@@ -0,0 +1,10 @@
+package dsacoursetim;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("I am going to learn Data Structures and Algorithms!");
+		System.out.println("Excited!!!");
+	}
+
+}
