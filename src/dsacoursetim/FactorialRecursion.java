7589d93a379be4179f7099ddfea21fe1f0123c9b
Cubimal Chik <shubhachakrala@gmail.com>
2025-09-17 14:03:07 -0400

    shell sort implementation

---
+++ src/dsacoursetim/FactorialRecursion.java
@@ -0,0 +1,29 @@
+package dsacoursetim;
+
+public class FactorialRecursion {
+	
+	public static void main(String[] args) {
+		int num =7 ;
+		System.out.println(recursiveFactorial(num));
+	}
+	
+	public static int iterativeFactorial(int num) {
+		if(num == 0) {
+			return 1;
+		}
+		int factorial =1;
+		for(int i=1;i<=num;i++) {
+			factorial *=i;
+		}
+		return factorial;
+	}
+	
+	public static int recursiveFactorial(int num) {
+		if(num == 0) {
+			return 1;
+		}
+		int factorial = num*recursiveFactorial(num-1);
+		return factorial;
+	}
+
+}
---
+++ src/sorting/algos/ShellSort.java
@@ -0,0 +1,27 @@
+package sorting.algos;
+
+public class ShellSort {
+	
+	public static void main(String[] args) {
+		int[] array = { 20, 35, -15, 7, 55, 1, -22 };
+		shellSort(array);
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+	}
+
+	private static void shellSort(int[] array) {
+		for(int gap = array.length/2;gap >0;gap/=2) {
+			for(int i=gap;i<array.length;i++) {
+				int newElement = array[i];
+				int j=i;
+				while(j >= gap && array[j-gap] > newElement) {
+					array[j]=array[j-gap];
+					j-=gap;
+				}
+				array[j] = newElement;
+			}
+		}
+	}
+
+}
